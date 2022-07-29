d266d8530494ac3f55446114d308594dd1184ec5
bassemtarek <bassemtarek514@gmail.com>
2022-07-29 21:15:48 +0200

    Operations On Actors

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -6,13 +6,21 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name ="Profile Picture")]
+
+        [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
+
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50,MinimumLength =3, ErrorMessage ="Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
+
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
+        [MaxLength(450)]
         public string Bio { get; set; }
-        //Relationships
         public List<Actor_Movie> Actors_Movies { get; set; }
     }
 }
+
