9d3e107239e5100d2d4a1bf2e14e08256ba802d6
bassemtarek <bassemtarek514@gmail.com>
2022-12-05 17:19:47 +0200

    Identity-Adding logging in

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,6 +1,9 @@
 using eTickets.Data;
 using eTickets.Data.Cart;
 using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,9 +21,18 @@ builder.Services.AddScoped<IProducersService, ProducersService>();
 builder.Services.AddScoped<ICinemasService, CinemasService>();
 builder.Services.AddScoped<IMoviesService, MoviesService>();
 builder.Services.AddScoped<IOrdersService, OrdersService>();
+
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+
+//Authentication and Authorization Configuration
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
 builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
 
 var app = builder.Build();
 
@@ -37,6 +49,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.UseSession();
+//Authentication and Authorization 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAuthorization();
 
@@ -46,5 +61,6 @@ app.MapControllerRoute(
 
 //seed database
 AppDbInitializer.Seed(app);
+AppDbInitializer.SeedUsersAndRoles(app).Wait();
 
 app.Run();
